d8a58596825741c879ece66cf252a96221a22130
Dmitriy <dima.Nackonechnyi@gmail.com>
2019-06-24 18:20:23 +0300

    scrutor

--- source/repos/BlackJack/BlackJack.BusinessLogic/Configurations/DependencyConfiguration.cs
+++ source/repos/BlackJack/BlackJack.BusinessLogic/Configurations/DependencyConfiguration.cs
@@ -5,11 +5,8 @@ using BlackJack.BusinessLogic.Services.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using BlackJack.DataAccess.Repositories.Interfaces;
-using entity = BlackJack.DataAccess.Repositories.EntityFramework;
-using dapper = BlackJack.DataAccess.Repositories.Dapper;
 using BlackJack.BusinessLogic.Helpers.Interfaces;
 using BlackJack.BusinessLogic.Helpers;
-
 
 namespace BlackJack.BusinessLogic.Configurations
 {
@@ -20,97 +17,52 @@ namespace BlackJack.BusinessLogic.Configurations
             var activeRepository = configuration.GetSection("ActiveRepository").Value;
             if (activeRepository == "Dapper")
             {
-                services.Scan(scan => scan
-                .FromAssemblyOf<IBotRepository>()
-                .AddClasses(classes => classes.AssignableTo<dapper.BotRepository>())
-                .AsImplementedInterfaces()
-                .WithTransientLifetime()
-                .FromAssemblyOf<IBotStepRepository>()
-                .AddClasses(classes => classes.AssignableTo<dapper.BotStepRepository>())
-                .AsImplementedInterfaces()
-                .WithTransientLifetime()
-                .FromAssemblyOf<ICardRepository>()
-                .AddClasses(classes => classes.AssignableTo<dapper.CardRepository>())
-                .AsImplementedInterfaces()
-                .WithTransientLifetime()
-                .FromAssemblyOf<IGameRepository>()
-                .AddClasses(classes => classes.AssignableTo<dapper.GameRepository>())
-                .AsImplementedInterfaces()
-                .WithTransientLifetime()
-                .FromAssemblyOf<IPlayerRepository>()
-                .AddClasses(classes => classes.AssignableTo<dapper.PlayerRepository>())
-                .AsImplementedInterfaces()
-                .WithTransientLifetime()
-                .FromAssemblyOf<IPlayerStepRepository>()
-                .AddClasses(classes => classes.AssignableTo<dapper.PlayerStepRepository>())
-                .AsImplementedInterfaces()
-                .WithTransientLifetime()
-                .FromAssemblyOf<IBotInGameRepository>()
-                .AddClasses(classes => classes.AssignableTo<dapper.BotInGameRepository>())
-                .AsImplementedInterfaces()
-                .WithTransientLifetime()
-                .FromAssemblyOf<IPlayerInGameRepository>()
-                .AddClasses(classes => classes.AssignableTo<dapper.PlayerInGameRepository>())
-                .AsImplementedInterfaces()
-                .WithTransientLifetime());
+                services.Scan(scan =>
+                 scan.FromCallingAssembly()
+                 .FromAssemblies(
+                   typeof(IBotRepository).Assembly,
+                   typeof(IBotStepRepository).Assembly,
+                   typeof(ICardRepository).Assembly,
+                   typeof(IGameRepository).Assembly,
+                   typeof(IPlayerRepository).Assembly,
+                   typeof(IPlayerStepRepository).Assembly,
+                   typeof(IBotInGameRepository).Assembly,
+                   typeof(IPlayerInGameRepository).Assembly
+               )
+               .AddClasses(classes => classes.InNamespaces("BlackJack.DataAccess.Repositories.Dapper"))
+               .AsImplementedInterfaces()
+               .WithTransientLifetime());
             }
             if (activeRepository == "Entity")
             {
-                services.Scan(scan => scan
-                 .FromAssemblyOf<IBotRepository>()
-                 .AddClasses(classes => classes.AssignableTo<entity.BotRepository>())
-                 .AsImplementedInterfaces()
-                 .WithTransientLifetime()
-                 .FromAssemblyOf<IBotStepRepository>()
-                 .AddClasses(classes => classes.AssignableTo<entity.BotStepRepository>())
-                 .AsImplementedInterfaces()
-                 .WithTransientLifetime()
-                 .FromAssemblyOf<ICardRepository>()
-                 .AddClasses(classes => classes.AssignableTo<entity.CardRepository>())
-                 .AsImplementedInterfaces()
-                 .WithTransientLifetime()
-                 .FromAssemblyOf<IGameRepository>()
-                 .AddClasses(classes => classes.AssignableTo<entity.GameRepository>())
-                 .AsImplementedInterfaces()
-                 .WithTransientLifetime()
-                 .FromAssemblyOf<IPlayerRepository>()
-                 .AddClasses(classes => classes.AssignableTo<entity.PlayerRepository>())
-                 .AsImplementedInterfaces()
-                 .WithTransientLifetime()
-                 .FromAssemblyOf<IPlayerStepRepository>()
-                 .AddClasses(classes => classes.AssignableTo<entity.PlayerStepRepository>())
-                 .AsImplementedInterfaces()
-                 .WithTransientLifetime()
-                 .FromAssemblyOf<IBotInGameRepository>()
-                 .AddClasses(classes => classes.AssignableTo<entity.BotInGameRepository>())
-                 .AsImplementedInterfaces()
-                 .WithTransientLifetime()
-                 .FromAssemblyOf<IPlayerInGameRepository>()
-                 .AddClasses(classes => classes.AssignableTo<entity.PlayerInGameRepository>())
-                 .AsImplementedInterfaces()
-                 .WithTransientLifetime());
+                services.Scan(scan =>
+                 scan.FromCallingAssembly()
+                 .FromAssemblies(
+                   typeof(IBotRepository).Assembly,
+                   typeof(IBotStepRepository).Assembly,
+                   typeof(ICardRepository).Assembly,
+                   typeof(IGameRepository).Assembly,
+                   typeof(IPlayerRepository).Assembly,
+                   typeof(IPlayerStepRepository).Assembly,
+                   typeof(IBotInGameRepository).Assembly,
+                   typeof(IPlayerInGameRepository).Assembly
+               )
+               .AddClasses(classes => classes.InNamespaces("BlackJack.DataAccess.Repositories.EntityFramework"))
+               .AsImplementedInterfaces()
+               .WithTransientLifetime());
             }
-            services.Scan(scan => scan
-            .FromAssemblyOf<IAccountService>()
-            .AddClasses(classes => classes.AssignableTo<AccountService>())
-            .AsImplementedInterfaces()
-            .WithTransientLifetime()
-            .FromAssemblyOf<IGameService>()
-            .AddClasses(classes => classes.AssignableTo<GameService>())
-            .AsImplementedInterfaces()
-            .WithTransientLifetime()
-            .FromAssemblyOf<IHistoryService>()
-            .AddClasses(classes => classes.AssignableTo<HistoryService>())
-            .AsImplementedInterfaces()
-            .WithTransientLifetime()
-            .FromAssemblyOf<IJwtProvider>()
-            .AddClasses(classes => classes.AssignableTo<JwtProvider>())
-            .AsImplementedInterfaces()
-            .WithTransientLifetime()
-            .FromAssemblyOf<ICardHelper>()
-            .AddClasses(classes => classes.AssignableTo<CardHelper>())
-            .AsImplementedInterfaces()
-            .WithTransientLifetime());
+            services.Scan(scan =>
+                scan.FromCallingAssembly()
+                .FromAssemblies(
+                  typeof(IAccountService).Assembly,
+                  typeof(IGameService).Assembly,
+                  typeof(IJwtProvider).Assembly,
+                  typeof(IHistoryService).Assembly,
+                  typeof(ICardHelper).Assembly
+              )
+              .AddClasses()
+              .AsImplementedInterfaces()
+              .WithTransientLifetime());
         }
     }
 }
--- source/repos/BlackJack/BlackJack.BusinessLogic/Services/HistoryService.cs
+++ source/repos/BlackJack/BlackJack.BusinessLogic/Services/HistoryService.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Identity;
 using System.Linq;
 using BlackJack.ViewModels.HistoryViews;
 using System;
-using Microsoft.AspNetCore.Http;
 
 namespace BlackJack.BusinessLogic.Services
 {
