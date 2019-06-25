561525341c2ae207fdada4fb54d79989c90ee964
Dmitriy <dima.Nackonechnyi@gmail.com>
2019-06-25 09:25:35 +0300

    scrutor

--- source/repos/BlackJack/BlackJack.BusinessLogic/Configurations/DependencyConfiguration.cs
+++ source/repos/BlackJack/BlackJack.BusinessLogic/Configurations/DependencyConfiguration.cs
@@ -1,12 +1,9 @@
-﻿using BlackJack.BusinessLogic.Providers;
-using BlackJack.BusinessLogic.Providers.Interfaces;
-using BlackJack.BusinessLogic.Services;
+﻿using BlackJack.BusinessLogic.Providers.Interfaces;
 using BlackJack.BusinessLogic.Services.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using BlackJack.DataAccess.Repositories.Interfaces;
 using BlackJack.BusinessLogic.Helpers.Interfaces;
-using BlackJack.BusinessLogic.Helpers;
 
 namespace BlackJack.BusinessLogic.Configurations
 {
@@ -15,8 +12,7 @@ namespace BlackJack.BusinessLogic.Configurations
         public static void AddDependencyConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
             var activeRepository = configuration.GetSection("ActiveRepository").Value;
-            if (activeRepository == "Dapper")
-            {
+
                 services.Scan(scan =>
                  scan.FromCallingAssembly()
                  .FromAssemblies(
@@ -29,31 +25,10 @@ namespace BlackJack.BusinessLogic.Configurations
                    typeof(IBotInGameRepository).Assembly,
                    typeof(IPlayerInGameRepository).Assembly
                )
-               .AddClasses(classes => classes.InNamespaces("BlackJack.DataAccess.Repositories.Dapper"))
+               .AddClasses(classes => classes.InNamespaces(activeRepository))
                .AsImplementedInterfaces()
-               .WithTransientLifetime());
-            }
-            if (activeRepository == "Entity")
-            {
-                services.Scan(scan =>
-                 scan.FromCallingAssembly()
-                 .FromAssemblies(
-                   typeof(IBotRepository).Assembly,
-                   typeof(IBotStepRepository).Assembly,
-                   typeof(ICardRepository).Assembly,
-                   typeof(IGameRepository).Assembly,
-                   typeof(IPlayerRepository).Assembly,
-                   typeof(IPlayerStepRepository).Assembly,
-                   typeof(IBotInGameRepository).Assembly,
-                   typeof(IPlayerInGameRepository).Assembly
-               )
-               .AddClasses(classes => classes.InNamespaces("BlackJack.DataAccess.Repositories.EntityFramework"))
-               .AsImplementedInterfaces()
-               .WithTransientLifetime());
-            }
-            services.Scan(scan =>
-                scan.FromCallingAssembly()
-                .FromAssemblies(
+               .WithTransientLifetime()
+               .FromAssemblies(
                   typeof(IAccountService).Assembly,
                   typeof(IGameService).Assembly,
                   typeof(IJwtProvider).Assembly,
