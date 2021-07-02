3b525e7e27069d199c0bcde6a6c520b9680ce9e7
Eugene Titov <eugenetitov>
2021-07-03 02:39:24 +0300

    updated angular from 7 to 8

--- source/repos/BlackJack/BlackJack.WEB/ClientApp/src/app/app-routing.module.ts
+++ source/repos/BlackJack/BlackJack.WEB/ClientApp/src/app/app-routing.module.ts
@@ -4,10 +4,10 @@ import { OnlyLoggedOut } from 'src/app/shared/guards/only-logged-out-users.guard
 import { OnlyLoggedIn } from 'src/app/shared/guards/only-logged-in-users.guard';
 
 const routes: Routes = [
-  {path: 'home',loadChildren: 'src/app/home/home.module#HomeModule',canActivate: [OnlyLoggedOut]},
-  {path: 'auth',loadChildren: 'src/app/auth/auth.module#AuthModule',canActivate: [OnlyLoggedOut]},
-  {path: 'game',loadChildren: 'src/app/game/game.module#GameModule',canActivate: [OnlyLoggedIn]},
-  {path: 'history',loadChildren: 'src/app/history/history.module#HistoryModule',canActivate: [OnlyLoggedIn]},
+  {path: 'home', loadChildren: () => import('src/app/home/home.module').then(m => m.HomeModule), canActivate: [OnlyLoggedOut]},
+  {path: 'auth', loadChildren: () => import('src/app/auth/auth.module').then(m => m.AuthModule), canActivate: [OnlyLoggedOut]},
+  {path: 'game', loadChildren: () => import('src/app/game/game.module').then(m => m.GameModule), canActivate: [OnlyLoggedIn]},
+  {path: 'history', loadChildren: () => import('src/app/history/history.module').then(m => m.HistoryModule), canActivate: [OnlyLoggedIn]},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
